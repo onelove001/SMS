afc393a6437442f5ff56ecabb02c6faad64e0108
horlamy <horlamy007@gmail.com>
2022-02-19 00:23:45 +0100

    initial commit

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -120,7 +120,7 @@ AUTHENTICATION_BACKENDS = ['student_management_app.EmailBackend.EmailBackend']
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
-STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
